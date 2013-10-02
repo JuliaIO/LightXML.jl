af8f25241250e6a164ac25948c7a31416bb067e0
Dahua Lin <lindahua@gmail.com>
2013-10-01 21:53:58 -0500

    add nodes.jl and node types

--- src/errors.jl
+++ src/errors.jl
@@ -6,3 +6,25 @@ immutable XMLParseError <: XMLError
 end
 
 
+const dom_exception_causes = [
+	"Index size error",        #  1
+	"DOM string size error",   #  2
+	"Hierarchy request error", #  3
+	"Wrong document",          #  4
+	"Invalid character",       #  5
+	"No data allowed",         #  6
+	"No modification allowed", #  7
+	"Not found",               #  8
+	"Not supported",           #  9
+	"Inused attribute"         # 10       
+]
+
+
+immutable DOMException <: XMLError
+	code::Int
+	cause::ASCIIString
+
+	DOMException(code::Int) = new(code, dom_exception_causes[code])
+end
+
+
---
+++ src/nodes.jl
@@ -0,0 +1,41 @@
+# XML nodes
+
+
+#######################################
+#
+#  Node Type
+#
+#######################################
+
+const ELEMENT_NODE = 1
+const ATTRIBUTE_NODE = 2
+const TEXT_NODE = 3
+const CDATA_SECTION_NODE = 4
+const ENTITY_REF_NODE = 5
+const ENTITY_NODE = 6
+const PI_NODE = 7
+const COMMENT_NODE = 8
+const DOCUMENT_NODE = 9
+const DOCUMENT_TYPE_NODE = 10
+const DOCUMENT_FRAG_NODE = 11
+const NOTATION_NODE = 12
+const HTML_DOCUMENT_NODE = 13
+const DTD_NODE = 14
+const ELEMENT_DECL = 15
+const ATTRIBUTE_DECL = 16
+const ENTITY_DECL = 17
+const NAMESPACE_DECL = 18
+const XINCLUDE_START = 19
+const XINCLUDE_END = 20
+const DOCB_DOCUMENT_NODE = 21
+
+
+#######################################
+#
+#  Generic node interface
+#
+#######################################
+
+
+
+
