55eed4b0e95fd26173002256ba2d47b0d7897f66
Tony Kelman <tony@kelman.net>
2014-02-25 00:32:29 -0800

    Add libiconv dependency
    Needs to be under the -2 name until I can figure out how aliases work

--- deps/build.jl
+++ deps/build.jl
@@ -2,28 +2,37 @@ using BinDeps
 
 @BinDeps.setup
 
-libxml2 = library_dependency("libxml2", aliases=["libxml2-2"])
+deps = [
+        libiconv = library_dependency("libiconv-2", aliases = ["libiconv"])
+        libxml2 = library_dependency("libxml2", aliases = ["libxml2-2"], depends = [libiconv])
+        ]
 
-# Windows
-downloadsdir = BinDeps.downloadsdir(libxml2)
-libdir = BinDeps.libdir(libxml2)
-downloadname = WORD_SIZE == 32 ? "libxml2-2.9.1-win32-x86.7z" : "libxml2-2.9.1-win32-x86_64.7z"
+location = "ftp://ftp.zlatkovic.com/libxml/64bit/"
+suffix = WORD_SIZE == 32 ? "-win32-x64.7z" : "-win32-x86_64.7z"
+downloadnames = ["iconv-1.14", "libxml2-2.9.1"]
 
-# BinDeps complains about the .7z file on other platforms...
-@windows_only provides(BuildProcess,
-    (@build_steps begin
-        FileDownloader("ftp://ftp.zlatkovic.com/libxml/64bit/$downloadname", joinpath(downloadsdir, downloadname))
-	CreateDirectory(BinDeps.srcdir(libxml2), true)
-	FileUnpacker(joinpath(downloadsdir, downloadname), BinDeps.srcdir(libxml2), joinpath(BinDeps.srcdir(libxml2),"bin"))
-	CreateDirectory(libdir, true)
-	@build_steps begin
-	    ChangeDirectory(joinpath(BinDeps.srcdir(libxml2),"bin"))
-	    FileRule(joinpath(libdir,"libxml2.dll"), @build_steps begin
-	        `cp libxml2-2.dll $(libdir)/libxml2.dll`
-	    end)
-	end
-     end), libxml2, os = :Windows)
+for i=1:2
+    downloadsdir = BinDeps.downloadsdir(deps[i])
+    downloadname = "$(downloadnames[i])$suffix"
+    srcdir = joinpath(BinDeps.depsdir(deps[i]), downloadnames[i])
+    libdir = BinDeps.libdir(deps[i])
+    @windows_only provides(BuildProcess,
+        (@build_steps begin
+        FileDownloader("$location$downloadname", joinpath(downloadsdir, downloadname))
+        CreateDirectory(srcdir, true)
+        FileUnpacker(joinpath(downloadsdir, downloadname), srcdir, "bin")
+        CreateDirectory(libdir, true)
+        @build_steps begin
+            ChangeDirectory(joinpath(srcdir, "bin"))
+            FileRule(joinpath(libdir, "$(deps[i].name).dll"), @build_steps begin
+                `cp $(deps[i].name)*.dll $(libdir)/$(deps[i].name).dll`
+            end)
+        end
+        end), deps[i], os = :Windows)
+end
 
 @windows_only push!(BinDeps.defaults, BuildProcess)
+
+@BinDeps.install
 
 @windows_only pop!(BinDeps.defaults)
