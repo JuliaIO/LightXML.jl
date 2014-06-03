dd22ca70d614decfc38d44de2b85ee742f50379d
Dahua Lin <lindahua@gmail.com>
2014-06-03 16:09:13 -0500

    minor change for windows dep

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinDeps
 @BinDeps.setup
 
 @windows_only begin
-libxml2 = library_dependency("libxml2", aliases = ["libxml2-2"])
+libxml2 = library_dependency("libxml2-2")
 using WinRPM
 provides(WinRPM.RPM, "libxml2", libxml2, os = :Windows)
 end
