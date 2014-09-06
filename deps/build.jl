9bde3c57b5994443affb669896486b0ef83b2be0
Tony Kelman <tony@kelman.net>
2014-09-06 07:33:45 -0700

    fix WinRPM package name
    see https://build.opensuse.org/package/rdiff/windows:mingw:win64/mingw64-libxml2?linkrev=base&rev=29, dll was moved into a separately named package

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ using BinDeps
 
 libxml2 = library_dependency("libxml2-2")
 using WinRPM
-provides(WinRPM.RPM, "libxml2", libxml2, os = :Windows)
+provides(WinRPM.RPM, "libxml2-2", libxml2, os = :Windows)
 
 @BinDeps.install
 end
