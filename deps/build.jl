e0427e4d45629c5151011a563121e6caf0df19d0
Tony Kelman <tony@kelman.net>
2015-07-14 23:22:51 -0700

    adjust bindeps usage
    trying to fix win32 problem

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,11 @@
-using BinDeps
+using BinDeps, Compat
 
 @BinDeps.setup
 
+libxml2 = library_dependency("libxml2", aliases="libxml2-2")
 @windows_only begin
-    libxml2 = library_dependency("libxml2-2")
     using WinRPM
     provides(WinRPM.RPM, "libxml2-2", libxml2, os = :Windows)
 end
 
-@BinDeps.install
+@BinDeps.install @compat(Dict(:libxml2 => :libxml2))
