56753f69dbbba59139c41cb60a317a72b6244e74
Tony Kelman <tony@kelman.net>
2015-07-15 08:46:11 -0400

    drop bindeps, always install from winrpm directly

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,4 @@
-using BinDeps, Compat
-
-@BinDeps.setup
-
-libxml2 = library_dependency("libxml2", aliases="libxml2-2")
 @windows_only begin
     using WinRPM
-    provides(WinRPM.RPM, "libxml2-2", libxml2, os = :Windows)
+    WinRPM.install("libxml2-2", yes=true)
 end
-
-@BinDeps.install @compat(Dict(:libxml2 => :libxml2))
