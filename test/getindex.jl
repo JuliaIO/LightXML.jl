da81f6c7a0f7657103c605646866e8f5a6572f4c
Sam O'Connor <sam@octech.com.au>
2016-01-02 18:36:13 +1100

    add xml_dict() and dict_xml(), convert XML to/from JSON-like dict

--- test/getindex.jl
+++ test/getindex.jl
@@ -1,4 +1,12 @@
-xml = """
+function xdict(xml)
+    for (n,v) in xml_dict(xml; strip_text=true)
+        if !isa(n, Symbol)
+            return v
+        end
+    end
+end
+
+xml1 = """
 <CreateQueueResponse>
     <CreateQueueResult>
         <QueueUrl>
@@ -13,11 +21,15 @@ xml = """
 </CreateQueueResponse>
 """
 
-@test parse_string(xml)["CreateQueueResult"]["QueueUrl"] ==
+@test parse_string(xml1)["CreateQueueResult"]["QueueUrl"] ==
+      "http://queue.amazonaws.com/123456789012/testQueue"
+
+@test xdict(xml1)["CreateQueueResult"]["QueueUrl"] ==
       "http://queue.amazonaws.com/123456789012/testQueue"
 
 
-xml = """
+
+xml2 = """
 <GetUserResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
   <GetUserResult>
     <User>
@@ -33,10 +45,14 @@ xml = """
 </GetUserResponse>
 """
 
-@test parse_string(xml)["GetUserResult"]["User"]["Arn"] == "arn:aws:iam::012541411202:root"
+@test parse_string(xml2)["GetUserResult"]["User"]["Arn"] == 
+      "arn:aws:iam::012541411202:root"
+
+@test xdict(xml2)["GetUserResult"]["User"]["Arn"] == 
+      "arn:aws:iam::012541411202:root"
 
 
-xml = """
+xml3 = """
 <GetQueueAttributesResponse>
   <GetQueueAttributesResult>
     <Attribute>
@@ -82,14 +98,20 @@ xml = """
 </GetQueueAttributesResponse>
 """
 
-xml = parse_string(xml)
+xml = parse_string(xml3)
+d = [a["Name"] => a["Value"] for a in xml["GetQueueAttributesResult"]["Attribute"]]
+
+@test d["MessageRetentionPeriod"] == "345600"
+@test d["CreatedTimestamp"] == "1286771522"
+
+xml = xdict(xml3)
 d = [a["Name"] => a["Value"] for a in xml["GetQueueAttributesResult"]["Attribute"]]
 
 @test d["MessageRetentionPeriod"] == "345600"
 @test d["CreatedTimestamp"] == "1286771522"
 
 
-xml = """
+xml4 = """
 <?xml version="1.0" encoding="UTF-8"?>
 <ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01">
   <Owner>
@@ -109,10 +131,11 @@ xml = """
 </ListAllMyBucketsResult>
 """
 
-@test [b["Name"] for b in parse_string(xml)["Buckets"]["Bucket"]] == ["quotes", "samples"]
+@test [b["Name"] for b in parse_string(xml4)["Buckets"]["Bucket"]] == ["quotes", "samples"]
 
+@test [b["Name"] for b in xdict(xml4)["Buckets"]["Bucket"]] == ["quotes", "samples"]
 
-xml = """
+xml5 = """
 <ListDomainsResponse>
   <ListDomainsResult foobar="Hello">
     <DomainName>Domain1</DomainName>
@@ -126,4 +149,67 @@ xml = """
 </ListDomainsResponse>
 """
 
-@test parse_string(xml)["ListDomainsResult"][:foobar] == "Hello"
+@test parse_string(xml5)["ListDomainsResult"][:foobar] == "Hello"
+
+@test xdict(xml5)["ListDomainsResult"][:foobar] == "Hello"
+
+
+xml6 = """
+<?xml version="1.0" encoding="UTF-8"?>
+<bookstore brand="amazon">
+  <book category="COOKING" tag="first">
+    <title lang="en">
+        Everyday Italian
+    </title>
+    <author>Giada De Laurentiis</author>
+    <year>2005</year>
+    <price>30.00</price>
+    <extract copyright="NA">The <b>bold</b> word is <b><i>not</i></b> <i>italic</i>.</extract>
+  </book>
+  <book category="CHILDREN">
+    <title lang="en">Harry Potter</title>
+    <author>J K. Rowling</author>
+    <year>2005</year>
+    <price>29.99</price>
+    <foo><![CDATA[<sender>John Smith</sender>]]></foo>
+    <extract>Click <a href="foobar.com">right <b>here</b></a> for foobar.</extract>
+  </book>
+  <metadata>
+       <foo>hello!</foo>
+  </metadata>
+</bookstore>
+"""
+
+function normalise_xml(xml)
+    o,i,p = readandwrite(
+        `bash -c 'xmllint --format --nocdata - | sed s/\ xmlns=\".*\"//g' `)
+    write(i, xml)
+    close(i)
+    readall(o)
+end
+
+using JSON
+
+for xml in [xml1, xml2, xml3, xml4, xml5, xml6]
+
+    if normalise_xml(xml) != normalise_xml(dict_xml(xml_dict(xml)))
+
+        println(normalise_xml(dict_xml(xml_dict(xml))))
+
+
+        open(`xargs -0 node -e "var o; o = JSON.parse(process.argv[1]);
+                                var u; u = require('util'); 
+                                console.log(u.inspect(o, {depth:null, colors: true}));"
+              `, "w", STDOUT) do io
+            write(io, json(xml_dict(xml)))
+        end
+
+        open("/tmp/a", "w") do f write(f, normalise_xml(xml)) end
+        open("/tmp/b", "w") do f write(f, normalise_xml(dict_xml(xml_dict(xml)))) end
+        run(`opendiff /tmp/a /tmp/b`)
+    end
+
+
+    @test normalise_xml(xml) == normalise_xml(dict_xml(xml_dict(xml)))
+
+end
