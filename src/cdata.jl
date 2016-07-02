b263d35d1f21eccc9d2a0203a8236b9599a6ee1f
Stefan Karpinski <stefan@karpinski.org>
2016-07-02 09:33:57 -0400

    Compat update for String rename (#46)

--- src/cdata.jl
+++ src/cdata.jl
@@ -1,6 +1,6 @@
-function new_cdatanode(xdoc::XMLDocument, txt::ASCIIString)
+function new_cdatanode(xdoc::XMLDocument, txt::String)
         p = ccall((:xmlNewCDataBlock,libxml2), Xptr, (Xptr, Xstr, Cint), xdoc.ptr, txt, length(txt)+1)
         XMLNode(p)
 end
 
-add_cdata(xdoc::XMLDocument, x::XMLElement, txt::ASCIIString) = add_child(x, new_cdatanode(xdoc,txt))
+add_cdata(xdoc::XMLDocument, x::XMLElement, txt::String) = add_child(x, new_cdatanode(xdoc,txt))
