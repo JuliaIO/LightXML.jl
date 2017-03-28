5113c42d8b3831426ca7193a02f64ab2848268f5
Mus M <mus-m@outlook.com>
2017-03-28 10:56:22 -0400

    remove DOMException

--- src/errors.jl
+++ src/errors.jl
@@ -18,23 +18,3 @@ end
 immutable XMLTreeError{T<:AbstractString} <: XMLError
     msg::T
 end
-
-const dom_exception_causes = [
-    "Index size error",        #  1
-    "DOM string size error",   #  2
-    "Hierarchy request error", #  3
-    "Wrong document",          #  4
-    "Invalid character",       #  5
-    "No data allowed",         #  6
-    "No modification allowed", #  7
-    "Not found",               #  8
-    "Not supported",           #  9
-    "Inused attribute"         # 10
-]
-
-
-immutable DOMException{T<:AbstractString} <: XMLError
-    code::Int
-    cause::T
-end
-DOMException(code::Integer) = DOMException(code, dom_exception_causes[code])
