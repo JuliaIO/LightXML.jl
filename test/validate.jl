dfdbfaa68eb9957180eb73bfd5eb516c5f4081f8
Bill <wherrera10@users.noreply.github.com>
2020-09-18 07:15:45 -1000

    more  tests

--- test/validate.jl
+++ test/validate.jl
@@ -1,1 +1,13 @@
 @test validate("valid.xml", "valid.xsd") == true
+
+@test validate("valid.xml", "valid.xsd") == true
+
+doc = parse_file("valid.xml")
+schema = XMLSchema("valid.xsd")
+
+@test validate("valid.xml", schema) == true
+
+@test validate(doc, schema) == true
+
+@test validate(root(doc), schema) == true
+
