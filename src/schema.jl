b12eaf5198b459d984d2c5228b0fc3bb610a3bea
Bill <wherrera10@users.noreply.github.com>
2020-09-18 08:57:43 -1000

    documentation

--- src/schema.jl
+++ src/schema.jl
@@ -6,24 +6,39 @@ mutable struct XMLSchema
     isvalid::Bool
 end
 
+"""
+Create an XMLSchema from a C library schema context pointer
+"""
 function XMLSchema(context::Xptr)
     schema = ccall((:xmlSchemaParse, libxml2), Xptr, (Xptr,), context)
     schema != C_NULL || throw(XMLValidationError("Bad XML Schema in Document"))
     return XMLSchema(schema, true)
 end
 
+"""
+Create an XMLSchema from a file or url
+"""
 function XMLSchema(url::String)
     ctxt = ccall((:xmlSchemaNewParserCtxt, libxml2), Xptr, (Cstring,), url)
     ctxt != C_NULL || throw(XMLValidationError("Bad XML Schema at " * url))
     return XMLSchema(ctxt)
 end
 
+"""
+Create an XMLSchema from an XMLDocument
+"""
 function XMLSchema(doc::XMLDocument)
     ctxt = ccall((:xmlSchemaNewDocParserCtxt, libxml2), Xptr, (Xptr,), doc.ptr)
     ctxt != C_NULL || throw(XMLValidationError("Bad XML Schema in Document"))
     return XMLSchema(ctxt)
 end
 
+"""
+Validate an XMLDocument with an XMLSchema
+Returns true if valid
+    NB:  There might be a memory leak from the ctxt pointer here, but calling the libxml2 free context function 
+         to free ctxt causes a segfault in the library, so hopefully this is freed elsewhere by the C library
+"""
 function validate(xml::XMLDocument, schema::XMLSchema)
     ctxt = ccall((:xmlSchemaNewValidCtxt, libxml2), Xptr, (Xptr,), schema.ptr)
     err = ccall((:xmlSchemaValidateDoc, libxml2), 
@@ -31,6 +46,10 @@ function validate(xml::XMLDocument, schema::XMLSchema)
     return err == 0 ? true : false
 end
 
+"""
+Validate an XML file or url with an XMLSchema
+Returns true if valid
+"""
 function validate(url::String, schema::XMLSchema)
     ctxt = ccall((:xmlSchemaNewValidCtxt, libxml2), Xptr, (Xptr,), schema.ptr)
     err = ccall((:xmlSchemaValidateFile, libxml2), 
@@ -38,6 +57,10 @@ function validate(url::String, schema::XMLSchema)
     return err == 0 ? true : false
 end
 
+"""
+Validate an XMLElement of an XMLDocument with an XMLSchema
+Returns true if valid
+"""
 function validate(elem::XMLElement, schema::XMLSchema)
     ctxt = ccall((:xmlSchemaNewValidCtxt, libxml2), Xptr, (Xptr,), schema.ptr)
     err = ccall((:xmlSchemaValidateOneElement, libxml2), 
@@ -45,6 +68,10 @@ function validate(elem::XMLElement, schema::XMLSchema)
     return err == 0 ? true : false
 end
 
+"""
+Validate an XML file or url with an XSD file or url
+Returns true if valid
+"""
 function validate(url::String, schemafile::String)
     ctxt = ccall((:xmlSchemaNewParserCtxt, libxml2), Xptr, (Cstring,), schemafile)
     ctxt != C_NULL || throw(XMLValidationError("Bad XML Schema at " * schemafile))
